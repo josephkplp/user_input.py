60e59c6209dd09a76d26be83d168b42a867793fd
josephtheceo <159775398+josephkplp@users.noreply.github.com>
2024-08-18 21:07:03 +0300

    adding the user_input.py assignment

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# asking the user for thier name
+name = input("what is your name ? ")
+
+# asking the user for their age.
+age = input("hoe old are you? ")
+
+# asking the user for their locaton
+location = input("where do you live")
+
+# printing a personalized massage 
+print(f"hello {name}, it seems you are {age} and you live in {location}. ")
+ 
